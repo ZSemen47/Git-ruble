e226f363bd63e64075be8bff7e61cf4456d504c7
ZSemen47 <65822156+ZSemen47@users.noreply.github.com>
2020-09-21 22:27:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         int balance = 100;
         int transfer = 1100;
         int bonus = 0;
-        if (transfer >= 1100) {
+        if (transfer >= 1000) {
             bonus = transfer / 100;
         }
 
         int total_balance = balance + transfer + bonus;
-        System.out.println(total_balance);
+        System.out.println(totalbalance);
     }
 }
